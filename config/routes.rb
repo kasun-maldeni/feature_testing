1de729729557994199bbab6a445a3fe26bfd4c2e
Kasun <kasun.maldeni@gmail.com>
2018-08-29 16:42:35 +1000

    add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
   resources :todos, only: [:new, :create]
+  resource :session, only: [:new, :create]
+  get '/session/new', to: 'session#new', as: 'sign_in'
 end
